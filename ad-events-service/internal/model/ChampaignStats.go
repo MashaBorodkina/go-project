f6da984dda3201e516fd787983db394012651a64
Maria Borodkina <mashaborodkina1507@ya.ru>
2026-05-17 00:36:42 +0300

    fix: add fields CampaignName and Budget into CampaignStats model

--- ad-events-service/internal/model/ChampaignStats.go
+++ ad-events-service/internal/model/ChampaignStats.go
@@ -7,6 +7,8 @@ import (
 type CampaignStats struct {
 	CampaignID  uuid.UUID `json:"campaign_id"`
 	Impressions int       `json:"impressions"`
+	CampaignName string    `json:"campaign_name"`
+	Budget      float64   `json:"budget"`
 	Clicks      int       `json:"clicks"`
 	CTR         float64   `json:"ctr"` // Click-Through Rate	
 }
