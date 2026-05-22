5d50ef557896f8a68183f5bf6e8680a4754f0325
Maria Borodkina <mashaborodkina1507@ya.ru>
2026-05-22 17:18:37 +0300

    feat: adding errors to handle banner and campaign events

---
+++ ad-events-service/internal/errors/errors.go
@@ -0,0 +1,11 @@
+package errors
+
+import "errors"
+
+var (
+	ErrBannerNotFound =errors.New("banner not found")
+	ErrCampaignNotFound =errors.New("campaign not found")
+	ErrBannerInactive =errors.New("banner is inactive")
+	ErrCampaignInactive =errors.New("campaign is paused or archived")
+	ErrInvalidEventType =errors.New("invalid event type")
+)
