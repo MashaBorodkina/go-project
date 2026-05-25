3a256be0ba69fa8d420c0a86a636238dac7b8464
Maria Borodkina <mashaborodkina1507@ya.ru>
2026-05-25 12:56:52 +0300

    feat: implement EventService with event tracking and retrieval methods

---
+++ ad-events-service/internal/service/event_service.go
@@ -0,0 +1,102 @@
+package service
+
+import (
+	"ad-events-service/internal/errors"
+	"ad-events-service/internal/model"
+	"ad-events-service/internal/repository"
+	"context"
+	"fmt"
+)
+
+type EventService struct {
+	EventRepo *repository.EventRepository
+	BanRepo *repository.BannerRepository
+	CampRepo *repository.Repository
+}
+
+func NewEventService(eventRepo *repository.EventRepository, campRepo *repository.Repository, banRepo *repository.BannerRepository) *EventService {
+	return &EventService{
+		EventRepo: eventRepo,
+		CampRepo: campRepo,
+		BanRepo: banRepo,
+
+	}
+}
+
+func (s *EventService) TrackEvent(ctx context.Context, ban_id string, eventType string, ip string, userAgent string) error {
+	if ban_id == "" {
+		return fmt.Errorf("banner ID cannot be empty")
+	}
+	ban, err := s.BanRepo.GetBannerByID(ctx, ban_id)
+
+	if err !=nil {
+		return fmt.Errorf("failed to get banner by ID: %w", err)
+	}
+
+	if ban == nil {
+		return errors.ErrBannerNotFound
+	}
+	if ! ban.IsActive {
+		return errors.ErrBannerInactive
+	}
+
+	camp, err := s.CampRepo.GetCampaignByID(ctx, ban.CampaignID.String())
+
+	if err !=nil {
+		return fmt.Errorf("failed to get campaign by banner ID: %w", err)
+	}
+
+	if camp == nil {
+		return errors.ErrCampaignNotFound
+	}
+
+	if camp.Status != "active" {
+		return errors.ErrCampaignInactive
+	}
+
+	if eventType != "impression" && eventType != "click" {
+		return errors.ErrInvalidEventType
+	}
+
+	event := &model.Event{
+		Banner_ID: ban.ID,
+		Type: eventType,
+		Ip: ip,
+		User_Agent: userAgent,
+	}
+	if err := s.EventRepo.CreateEvent(ctx, event); err != nil {
+		return fmt.Errorf("failed to create event: %w", err)
+	}
+	return nil
+}
+
+func (s *EventService) TrackImpression(ctx context.Context, ban_id string, ip string, userAgent string) error {
+	return s.TrackEvent(ctx, ban_id, "impression", ip, userAgent)	
+}
+
+func (s *EventService) TrackClick(ctx context.Context, ban_id string, ip string, userAgent string) error {
+	return s.TrackEvent(ctx, ban_id, "click", ip, userAgent)
+}
+
+func (s *EventService) GetEventByID(ctx context.Context, ID string) (*model.Event, error) {
+	
+	if ID == "" {
+		return nil, fmt.Errorf("event ID cannot be empty")
+	}
+	event, err := s.EventRepo.GetEventByID(ctx, ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get event by ID: %w", err)
+	}
+	if event == nil {
+		return nil, errors.ErrEventNotFound
+	}
+	return event, nil
+}
+
+func (s *EventService) GetAllEvents(ctx context.Context) ([]*model.Event, error) {
+	events, err := s.EventRepo.GetAllEvents(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all events: %w", err)
+	}
+	return events, nil
+}
