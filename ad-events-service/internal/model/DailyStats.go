069fa27c8cf75f8ea2fd5497dcbc257f9ec81ca7
MashaBorodkina <mashaborodkina@mail.ru>
2026-06-03 17:59:21 +0300

    feat: add BannerID field to DailyStats struct

--- ad-events-service/internal/model/DailyStats.go
+++ ad-events-service/internal/model/DailyStats.go
@@ -4,6 +4,7 @@ import "time"
 
 type DailyStats struct {
 	Date        time.Time `json:"date"`
+	BannerID    string    `json:"banner_id,omitempty"`
 	Impressions int       `json:"impressions"`
 	Clicks      int       `json:"clicks"`
 	CTR         float64   `json:"ctr"` // Click-Through Rate
