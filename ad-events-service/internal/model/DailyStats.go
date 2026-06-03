a36fa15b00fc53c8729c0673eb6afe90061bdfa8
MashaBorodkina <mashaborodkina@mail.ru>
2026-06-03 18:03:41 +0300

    feat: remove BannerID field from DailyStats struct

--- ad-events-service/internal/model/DailyStats.go
+++ ad-events-service/internal/model/DailyStats.go
@@ -4,7 +4,6 @@ import "time"
 
 type DailyStats struct {
 	Date        time.Time `json:"date"`
-	BannerID    string    `json:"banner_id,omitempty"`
 	Impressions int       `json:"impressions"`
 	Clicks      int       `json:"clicks"`
 	CTR         float64   `json:"ctr"` // Click-Through Rate
