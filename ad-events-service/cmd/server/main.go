03921e59036de82169d61df47d0c88d764de81ff
MashaBorodkina <mashaborodkina@mail.ru>
2026-06-04 16:01:21 +0300

    feat: add routes for banners, stats, and events in main server file

--- ad-events-service/cmd/server/main.go
+++ ad-events-service/cmd/server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"ad-events-service/internal/handler"
+	"ad-events-service/internal/repository"
+	"ad-events-service/internal/service"
 	"context"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
@@ -30,4 +34,51 @@ func main() {
 		log.Fatalf("Unable to ping database: %v", err)
 	}
 	fmt.Println("Successfully connected to the database!")
+
+	campaignRepo := repository.NewRepository(pool)
+	bannerRepo := repository.NewBannerRepository(pool)
+	statsRepo := repository.NewStatsRepository(pool)
+	eventRepo := repository.NewEventRepository(pool)
+
+	campaignService := service.NewCampaignService(campaignRepo)
+	bannerService := service.NewBannerService(bannerRepo)
+	statsService := service.NewStatsService(statsRepo, campaignRepo)
+	eventService := service.NewEventService(eventRepo, campaignRepo, bannerRepo)
+	adService := service.NewAdService(bannerRepo, campaignRepo)
+
+	campaignHandler := handler.NewCampaignHandler(campaignService)
+	bannerHandler := handler.NewBannerHandler(bannerService)
+	statsHandler := handler.NewStatsHandler(statsService)
+	eventHandler := handler.NewEventHandler(eventService)
+	adHandler := handler.NewAdHandler(adService)
+
+	router := gin.Default()
+	router.GET("/campaigns", campaignHandler.GetAllCampaigns)
+	router.GET("/campaigns/:id", campaignHandler.GetCampaignByID)
+	router.POST("/campaigns", campaignHandler.CreateCampaign)
+	router.PUT("/campaigns/:id", campaignHandler.UpdateCampaign)
+	router.DELETE("/campaigns/:id", campaignHandler.DeleteCampaign)
+	router.PATCH("/campaigns/:id", campaignHandler.PatchCampaign)
+
+	router.GET("/banners", bannerHandler.GetAllBanners)
+	router.GET("/banners/:id", bannerHandler.GetBannerByID)
+	router.POST("/campaigns/:campaign_id/banners", bannerHandler.CreateBanner)
+	router.PUT("/banners/:id", bannerHandler.UpdateBanner)
+	router.DELETE("/banners/:id", bannerHandler.DeleteBanner)
+	router.PATCH("/banners/:id", bannerHandler.PatchBanner)
+
+	router.GET("/stats/banner/:id", statsHandler.GetBannerStatsByID)
+	router.GET("/stats/campaign/:id", statsHandler.GetCampaignStatsByID)
+	router.GET("/stats/campaign/:id/daily", statsHandler.GetDailyStats)
+
+	router.POST("/events", eventHandler.CreateEvent)
+	router.GET("/events/:event_id", eventHandler.GetEventByID)
+	router.GET("/events/banner/:banner_id/events", eventHandler.GetEventsByBannerID)
+	router.GET("/events/campaign/:campaign_id/events", eventHandler.GetEventsByCampaignID)
+
+	router.GET("/display", adHandler.GetBannerForDisplay)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
 }
