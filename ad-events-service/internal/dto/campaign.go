0413402f94b37b6dd9ffe862eed4af07788b7436
MashaB <mashaborodkina@mail.ru>
2026-06-22 15:04:13 +0200

    feat: add CreateCampaignResponse struct to handle campaign creation responses

--- ad-events-service/internal/dto/campaign.go
+++ ad-events-service/internal/dto/campaign.go
@@ -1,7 +1,17 @@
 package dto
 
+import "github.com/google/uuid"
+
 type PatchCampaignRequest struct {
 	Name   *string  `json:"name,omitempty"`
 	Budget *float64 `json:"budget,omitempty"`
 	Status *string  `json:"status,omitempty"`
+}
+
+type CreateCampaignResponse struct {
+	ID        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name" binding:"required"`
+	Budget    float64   `json:"budget" binding:"required,gt=0"`
+	Status    string    `json:"status" binding:"required,oneof=active paused archived"`
+	CreatedAt string    `json:"created_at,omitempty"`
 }
