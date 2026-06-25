83ff7d6fb735013220794ccc95d3a1e1d87c20e8
MashaB <mashaborodkina@mail.ru>
2026-06-25 13:48:07 +0200

    feat: add constants for event types and campaign/banner statuses

---
+++ ad-events-service/internal/model/constants.go
@@ -0,0 +1,13 @@
+package model
+
+const (
+	EventTypeImpression = "impression"
+	EventTypeClick      = "click"
+
+	CampaignStatusActive   = "active"
+	CampaignStatusPaused   = "paused"
+	CampaignStatusArchived = "archived"
+
+	BannerStatusActive   = "active"
+	BannerStatusInactive = "inactive"
+)
